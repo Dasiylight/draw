8ee635fac7d7b034aaa3343d55802a3d57bcd3b0
Dasiylight <zhiqinlong@foxmail.com>
2022-01-24 20:54:59 +0800

    a

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import registerElement from './register-element';
-// import ElementPlus from 'element-plus'
-
-import "element-plus/dist/index.css";
-
-
-createApp(App).use(registerElement).mount('#app')
