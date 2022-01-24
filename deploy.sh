1379e000abd37af32168d4c9b4baa10112e082bc
Dasiylight <zhiqinlong@foxmail.com>
2022-01-24 21:26:13 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -14,9 +14,9 @@ git add -A
 git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:Dasiylight/dasiylight.github.io.git/draw/ main
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Dasiylight/draw.git main:gh-pages
+# git push -f git@github.com:Dasiylight/draw.git main:gh-pages
 
 cd -
